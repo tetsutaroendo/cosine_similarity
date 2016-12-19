11d2a82b5b2399cc92a4abf1f653d4e9305fbe0e
Tetsutaro Endo <tetsutaro.endo@gaiax.com>
2016-12-19 23:11:33 +0900

    Fix data

--- cosine_similarity.rb
+++ cosine_similarity.rb
@@ -22,25 +22,18 @@ module CosineSimilarity
     CosineSimilarity.number(m_i, m_j) / CosineSimilarity.denom(m_i, m_j)
   end
 
-  def combination_calc(vector_hash)
-    vector_hash.keys.combination(2) do |a, b|
-      puts "#{a}-#{b} = #{CosineSimilarity.sim(vector_hash[a.to_sym], vector_hash[b.to_sym])}"
+  def combination_calc(vectors)
+    vectors.keys.combination(2) do |v1, v2|
+      puts "#{v1} - #{v2} = #{CosineSimilarity.sim(vectors[v1.to_sym], vectors[v2.to_sym])}"
     end
   end
 end
 
-
-vectors1 = {
-  :sample_vector1 => [3, 2, 1, 1],
-  :sample_vector2 => [2, 2, 1, 0],
-  :sample_vector3 => [1, 1, 2, 1],
-  :sample_vector4 => [1, 0, 1, 3]
+writers = {
+  :kotaro_isaka => [3, 2, 1, 1],
+  :keigo_higashino => [2, 2, 1, 0],
+  :haruki_murakami => [1, 1, 2, 1],
+  :j_k_rowling => [1, 0, 1, 3]
 }
 
-vectors2 = {
-  :sample_vector5 => [2, 2, 2, 2, 2, 2, 2, 0, 1, 1, 0 ,0],
-  :sample_vector6 => [0, 0, 0, 2, 2, 0, 2, 2, 2, 2, 1 ,1]
-}
-
-CosineSimilarity.combination_calc(vectors1)
-CosineSimilarity.combination_calc(vectors2)
+CosineSimilarity.combination_calc(writers)
