0b41d4b1e80208253ba3d64da14b30c505190d99
Tetsutaro Endo <tetsutaro.endo@gaiax.com>
2016-12-19 18:43:45 +0900

    Reviewed

--- cosine_similarity.rb
+++ cosine_similarity.rb
@@ -1,37 +1,30 @@
-class CosineSimilarity
-  class << self
+module CosineSimilarity
 
-    def numer(m_i, m_j)
-      result = 0.0
-      m_i.each_with_index do |m_i, t|
-        result += m_i * m_j[t]
-      end
-      result
+  module_function
+
+  def number(m_i, m_j)
+    result = 0.0
+    0.upto(m_i.length-1) do |i|
+      result += m_i[i] * m_j[i]
     end
+    result
+  end
 
-    def square_root(m)
-      result_m = 0.0
-      m.each do |m|
-        result_m += m ** 2.0
-      end
-      Math.sqrt(result_m)
-    end
+  def square_root(m)
+    Math.sqrt(m.inject(0.0){|sum, f| sum + (f ** 2.0)})
+  end
 
-    def denom(m_i, m_j)
-      CosineSimilarity.square_root(m_i) * CosineSimilarity.square_root(m_j)
-    end
+  def denom(m_i, m_j)
+    CosineSimilarity.square_root(m_i) * CosineSimilarity.square_root(m_j)
+  end
 
-    def sim(m_i, m_j)
-      CosineSimilarity.numer(m_i, m_j) / CosineSimilarity.denom(m_i, m_j)
-    end
+  def sim(m_i, m_j)
+    CosineSimilarity.number(m_i, m_j) / CosineSimilarity.denom(m_i, m_j)
+  end
 
-    def combination_calc(hash)
-      vector_names = hash.keys
-      for i in 0..vector_names.length-1
-        for j in i+1..vector_names.length-1
-          puts "#{vector_names[i]}-#{vector_names[j]} = #{CosineSimilarity.sim(hash[vector_names[i].to_sym], hash[vector_names[j].to_sym])}"
-        end
-      end
+  def combination_calc(vector_hash)
+    vector_hash.keys.combination(2) do |a, b|
+      puts "#{a}-#{b} = #{CosineSimilarity.sim(vector_hash[a.to_sym], vector_hash[b.to_sym])}"
     end
   end
 end
